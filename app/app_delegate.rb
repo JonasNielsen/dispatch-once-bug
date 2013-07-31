07deb3b262ebba474fe8f2fc6475d596781b9cda
Jonas Bruun Nielsen <jonas@jonasbnielsen.dk>
2013-07-31 19:31:44 +0200

    Added comment to app_delegate.rb

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,8 +2,8 @@ class AppDelegate
   def applicationDidFinishLaunching notification
     p "Both classes should return a singleton instance"
     p "OneSingletonClass.instance:"
-    p OneSingletonClass.instance
+    p OneSingletonClass.instance # returns instance correctly
     p "AnotherSingletonClass.instance:"
-    p AnotherSingletonClass.instance
+    p AnotherSingletonClass.instance # returns nil
   end
 end
